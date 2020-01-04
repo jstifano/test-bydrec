665f753e7ac4f73d409856e1de89b972bdb9aebf
Javier Stifano <jstifano18@gmail.com>
2020-01-04 17:28:40 -0400

    Util file to include all the functions needed in the whole API

---
+++ utils/Validation.js
@@ -0,0 +1,35 @@
+
+const REGEX_LETTERS = /[a-zA-Z]+/g;
+
+/**
+ * Utility method to make the validation of only letters
+ * @param {*} value --> Value to validate if are only letters 
+ */
+const isOnlyLetters = (value) => {
+    let result = REGEX_LETTERS.test(value) ? true : false;
+    return result;
+}
+
+/**
+ * Utility mehtod to create the date as MM/DD/YY 
+ * @param {*} value --> Value to validate if are only letters 
+ */
+const createDateFormatted = () => {
+    let d = new Date
+    let dformat = 
+    [
+        d.getMonth() + 1, 
+        d.getDate(), 
+        d.getFullYear().substr(d.getFullYear().length - 2)
+    ].join('/') +' '+ 
+    [
+        d.getHours(),
+        d.getMinutes(),
+        d.getSeconds()
+    ].join(':');
+    return dformat;
+}
+
+module.exports = {
+    isOnlyLetters: isOnlyLetters
+}
